87f68045c46fe0db08f65686b80768e88f182a8d
blueheeya83 <blueheeya12@gmail.com>
2024-07-04 00:33:55 +0900

    mainDiary layout

--- src/page/Home.js
+++ src/page/Home.js
@@ -32,11 +32,36 @@ function Home() {
             </ul>
             {/* E : 캠핑장 분류 */}
             {/* S : 별숲 기록 */}
-            <div className="titelWrap cntWarpPadding wrapMarginBottom">
+            <div className="titleWrap cntWarpPadding wrapMarginBottom">
                 <h3>별숲 기록</h3>
                 <Link to={"/store/view"}>전체보기</Link>
             </div>
-            <div className="mainDiaryWrap"></div>
+            <div className="mainDiaryWrap cntWarpPadding cntMarginBottom">
+                <div className="mainDiaryCard">
+                    <div className="imgWrap">
+                        <img src="" />
+                    </div>
+                    <ul className="textWrap">
+                        <li className="textTitle">
+                            시설 깨끗하고 아이들 놀거리가 많아요 ^^
+                        </li>
+                        <li className="textDate">오늘</li>
+                        <li className="textCamp">연천 평화강변 캠핑장</li>
+                    </ul>
+                </div>
+                <div className="mainDiaryCard">
+                    <div className="imgWrap">
+                        <img src="" />
+                    </div>
+                    <ul className="textWrap">
+                        <li className="textTitle">
+                            시설 깨끗하고 아이들 놀거리가 많아요 ^^
+                        </li>
+                        <li className="textDate">오늘</li>
+                        <li className="textCamp">연천 평화강변 캠핑장</li>
+                    </ul>
+                </div>
+            </div>
             {/* E : 별숲 기록 */}
             {/* S : 스토어 TOP10 */}
             <StoreTopTen className="cntMarginBottom" />
