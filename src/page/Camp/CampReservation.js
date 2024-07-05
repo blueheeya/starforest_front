34feea27b03f97d7a3f98e020a69da599fc48f8a
Kim-Dong-ill <ehddlf2893@naver.com>
2024-07-05 18:35:12 +0900

    css

--- src/page/Camp/CampReservation.js
+++ src/page/Camp/CampReservation.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Calender from "../../components/Camp/Calendar";
+import CampSelectCard from "../../components/Camp/CampSelectCard";
 
 function CampReservation() {
     return (
         <>
-            <div>CampReservation</div>
+            <CampSelectCard />
+            <Calender />
         </>
     );
 }
