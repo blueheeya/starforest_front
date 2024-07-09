63440256e001d190a2993f65f0b08969bd7558d9
Kim-Dong-ill <ehddlf2893@naver.com>
2024-07-09 18:57:49 +0900

    결제

--- src/components/Camp/CampPayMethod.js
+++ src/components/Camp/CampPayMethod.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import Naver from "../../assets/images/naverLogo.png"
 import kakao from "../../assets/images/kakaoLogo.png"
+import axios from 'axios';
 
 function CampPayMethod() {
+
+    const handleKakaoPayment = async () => {
+        try {
+            const res = await axios.post('http://localhost:8082/kakaoPay');
+            // 카카오페이 결제 페이지로 리다이렉트
+            window.location.href = res.data.next_redirect_pc_url;
+        } catch (error) {
+            console.error('결제 요청 중 오류 발생:', error);
+        }
+    }
+
     return (
         <div className="campPayMethod">
             <div className="methodName">결제 방법</div>
             <div className="methodKind">
-                <button className="kakao">
+                <button className="kakao" onClick={handleKakaoPayment}>
                     <img src={kakao} alt="" />
                     <div>카카오 pay</div>
                 </button>
--- src/page/Camp/CampReservationComplete.js
+++ src/page/Camp/CampReservationComplete.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import iconEdit from "../../assets/images/iconEdit.svg";
 import PayComplete from "../../components/Camp/PayComplete";
+import axios from "axios";
 
 function CampReservationComplete() {
+
   const contents = {
     title: "결제가 완료 되었습니다.",
     btn1: "캠핑장 더 알아보기",
     btn2: "예약장보 확인하기",
   };
+
+  const [paymentInfo, setPaymentInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchPaymentInfo = async () => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const pgToken = urlParams.get('pg_token');
+      console.log("pg토큰 : " + pgToken);
+      try {
+        const response = await axios.get(`http://localhost:8082/kakaoPaySuccess?pg_token=${pgToken}`);
+        setPaymentInfo(response.data);
+      } catch (error) {
+        console.error('결제 정보 조회 중 오류 발생:', error);
+      }
+    };
+
+    fetchPaymentInfo();
+  }, []);
 
   return (
     <>
