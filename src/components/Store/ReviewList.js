6de344ac57d250dfdd221cbc173f9085b36019b8
boram <qhfka676@naver.com>
2024-07-28 22:36:00 +0900

    store axios

--- src/components/Store/ReviewList.js
+++ src/components/Store/ReviewList.js
@@ -1,7 +1,49 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function ReviewList() {
-    return <div>ReviewList</div>;
+function ReviewList({ reviews, onDelete }) {
+  return (
+    <div className="reviewContent">
+      {reviews.map((review) => (
+        <div key={review.id} className="reviewItem">
+          <img
+            className="reviewuserImg"
+            src={`${process.env.REACT_APP_IMAGE_URL}/${review.img}`}
+            alt={`${review.user}의 프로필`}
+          />
+          <div className="reviewWrap">
+            <div className="reviewInfo">
+              <div className="reviewUser">
+                <p className={review.level}>{review.name}</p>
+                <p className="reviewUser">{review.user}</p>
+              </div>
+              <p className="reviewText">{review.content}</p>
+            </div>
+            <button
+              className="deleteReviewBtn"
+              onClick={() => onDelete(review.id)}
+            >
+              X
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
+
+ReviewList.propTypes = {
+  reviews: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      img: PropTypes.string.isRequired,
+      level: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      user: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
 
 export default ReviewList;
--- src/page/Store/StoreList.js
+++ src/page/Store/StoreList.js
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import EventSwiper from "../../components/Store/EventSwiper";
 import StoreTopTen from "../../components/Store/StoreTopTen";
 import { Link, useParams } from "react-router-dom";
 import "../../assets/css/storeStyle.scss";
-import { type } from "@testing-library/user-event/dist/type";
 import axiosInstance from "../../utils/axios";
 
 function StoreList() {
@@ -13,129 +12,56 @@ function StoreList() {
     { type: "푸드", StoreIcon: "캠핑 푸드", link: "/store/list" },
     { type: "가구", StoreIcon: "캠핑 가구", link: "/store/list" },
   ];
-  const products = [
-    {
-      id: 1,
-      brandName: "브랜드명1",
-      productName: "상품명블라블라1",
-      Category: "캠핑텐트",
-      price: 1000000,
-      image: "imgdefault.png",
-      sale: "30%",
-      starsale: "특가세일",
-      delivery: "무료배송",
-      link: "/store/view",
-    },
-    {
-      id: 2,
-      brandName: "브랜드명2",
-      productName: "상품명블라블라2",
-      Category: "캠핑텐트",
-      price: 1000000,
-      image: "imgdefault.png",
-      sale: "30%",
-      starsale: "특가세일",
-      delivery: "무료배송",
-      link: "/store/view",
-    },
-    {
-      id: 3,
-      brandName: "브랜드명3",
-      productName: "상품명블라블라3",
-      Category: "캠핑텐트",
-      price: 1000000,
-      image: "imgdefault.png",
-      sale: "30%",
-      starsale: "특가세일",
-      delivery: "무료배송",
-      link: "/store/view",
-    },
-    {
-      id: 4,
-      brandName: "브랜드명4",
-      productName: "상품명블라블라4",
-      Category: "캠핑가구",
-      price: 1000000,
-      image: "imgdefault.png",
-      sale: "30%",
-      starsale: "특가세일",
-      delivery: "무료배송",
-      link: "/store/view",
-    },
-    {
-      id: 5,
-      brandName: "브랜드명5",
-      productName: "상품명블라블라5",
-      Category: "캠핑가구",
-      price: 1000000,
-      image: "imgdefault.png",
-      sale: "30%",
-      starsale: "특가세일",
-      delivery: "무료배송",
-      link: "/store/view",
-    },
-    {
-      id: 6,
-      brandName: "브랜드명6",
-      productName: "상품명블라블라6",
-      Category: "캠핑가구",
-      price: 1000000,
-      image: "imgdefault.png",
-      sale: "30%",
-      starsale: "특가세일",
-      delivery: "무료배송",
-      link: "/store/view",
-    },
-    {
-      id: 7,
-      brandName: "브랜드명7",
-      productName: "상품명블라블라7",
-      Category: "캠핑푸드",
-      price: 1000000,
-      image: "imgdefault.png",
-      sale: "30%",
-      starsale: "특가세일",
-      delivery: "무료배송",
-      link: "/store/view",
-    },
-    {
-      id: 8,
-      brandName: "브랜드명8",
-      productName: "상품명블라블라8",
-      Category: "캠핑푸드",
-      price: 1000000,
-      image: "imgdefault.png",
-      sale: "30%",
-      starsale: "특가세일",
-      delivery: "무료배송",
-      link: "/store/view",
-    },
-    {
-      id: 9,
-      brandName: "브랜드명9",
-      productName: "상품명블라블라9",
-      Category: "캠핑푸드",
-      price: 1000000,
-      image: "imgdefault.png",
-      sale: "30%",
-      starsale: "특가세일",
-      delivery: "무료배송",
-      link: "/store/view",
-    },
-  ];
 
   const { type } = useParams();
-  const [activeCategory, setActiveCategory] = useState("전체");
-  const [typeData, setTypeData] = useState([]);
+  const [activeCategory, setActiveCategory] = useState(type || "전체");
+  const [storeData, setStoreData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [hasMore, setHasMore] = useState(true);
 
   const handleCategoryClick = (category) => {
     setActiveCategory(category.type);
+    setStoreData([]);
+    setHasMore(true);
+    fetchStoreInfo({ skip: 0, limit: 10, filters: { type: category.type } });
   };
 
-  const filteredProducts =
-    activeCategory === "전체"
-      ? products
-      : products.filter((product) => product.storeCategory === activeCategory);
+  async function fetchStoreInfo({
+    skip,
+    limit,
+    loadMore = false,
+    filters = {},
+  }) {
+    try {
+      setLoading(true);
+      const params = { skip, limit, ...filters };
+
+      if (activeCategory !== "전체") {
+        params.type = activeCategory;
+      }
+
+      const res = await axiosInstance.get("/store/list", { params });
+
+      console.log("API Response:", res); // 응답 데이터 구조 확인
+
+      if (res.data && res.data.store) {
+        setStoreData((prevData) =>
+          loadMore ? [...prevData, ...res.data.store] : res.data.store
+        );
+        setHasMore(res.data.hasMore);
+      } else {
+        console.error("Invalid response format", res);
+      }
+      setLoading(false);
+    } catch (e) {
+      console.error("Error fetching store data!", e);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchStoreInfo({ skip: 0, limit: 10, filters: { type: activeCategory } });
+  }, [activeCategory]);
 
   return (
     <>
@@ -143,9 +69,9 @@ function StoreList() {
       <StoreTopTen className="cntMarginBottom" />
 
       <ul className="storeCategory">
-        {storeCategory.map((category, index) => (
+        {storeCategory.map((category) => (
           <li
-            key={index}
+            key={category.type}
             className={`scIconWrap ${
               activeCategory === category.type ? "active" : ""
             }`}
@@ -159,35 +85,33 @@ function StoreList() {
       </ul>
 
       <div className="storeList">
-        {products.map((products) => (
-          //   <Link to={`/store/view/${products.id}`} key={products.id}>
-          <Link to={products.link}>
-            <div key={products.id} className="product-item">
+        {storeData.map((product) => (
+          <Link to={`/store/view/${product.id}`} key={product.id}>
+            <div className="product-item">
               <div className="productImg">
                 <img
-                  src={
-                    process.env.PUBLIC_URL + `/assets/images/${products.image}`
-                  }
-                  alt=""
+                  src={`${process.env.PUBLIC_URL}/assets/images/${product.image}`}
+                  alt={product.product_name}
                 />
               </div>
               <div className="storeNameWrap">
-                <div className="Bname">{products.brandName}</div>
-                <div className="Pname">{products.productName}</div>
-                {/* <div className="Pname">{products.Category}</div> */}
+                <div className="Bname">{product.brand_name}</div>
+                <div className="Pname">{product.product_name}</div>
               </div>
               <div className="storePriceWrap">
-                <div className="sale">{products.sale}</div>
-                <div className="price">{products.price.toLocaleString()}원</div>
+                <div className="sale">{product.sale}</div>
+                <div className="price">{product.price.toLocaleString()}원</div>
               </div>
               <div className="storeEtc">
-                <span className="textIconType1">{products.starsale}</span>
-                <span className="textIconType2">{products.delivery}</span>
+                <span className="textIconType1">{product.starsale}</span>
+                <span className="textIconType2">{product.delivery}</span>
               </div>
             </div>
           </Link>
         ))}
       </div>
+      {loading && <p>Loading...</p>}
+      {!hasMore && <p>No more products</p>}
     </>
   );
 }
