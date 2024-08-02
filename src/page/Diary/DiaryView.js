fdb303636ac081a83dff1ebc69ec80b4bf546b08
chanjin97 <chanjindark@naver.com>
2024-08-02 18:41:36 +0900

    axios get 진행중

--- src/page/Diary/DiaryView.js
+++ src/page/Diary/DiaryView.js
@@ -1,11 +1,82 @@
-import React from "react";
-import diary_camp_img from "../../assets/images/diary_camp_img.svg";
-import Icon from "../../components/Icon/Icon";
+import React, { useEffect, useState } from "react";
 import UserTags from "../../components/Diary/UserTags";
 import CampSelectCard from "../../components/Camp/CampSelectCard";
 import UserCard from "../../components/User/UserCard";
+import HashTags from "../../components/Diary/HashTags";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function DiaryView() {
+  const [diary, setDiary] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const { id } = useParams(); // 아이디 Parma값으로 불러오기
+
+  // useEffect
+  useEffect(() => {
+    const fetchDiary = async () => {
+      try {
+        const res = await axios.get(`http://localhost:8080/diary/view/${id}`);
+        setDiary(res.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("error fetching diary", error);
+        setLoading(false);
+      }
+    };
+
+    fetchDiary();
+  }, [id]);
+
+  // 로딩중
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  // diary 에러
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  // diary 가 없을때
+  if (!diary) {
+    return <div>별숲기록을 찾지 못했습니다. 다시 시도해주세요.</div>;
+  }
+
+  const userTags = diary.allTags
+    ? diary.allTags
+        .split(",")
+        .filter((tag) =>
+          [
+            "매너타임",
+            "친절함",
+            "청결함",
+            "수영장",
+            "놀이시설",
+            "개별 화장실",
+            "개별 샤워실",
+            "매점 운영",
+          ].includes(tag)
+        )
+    : [];
+  const hashTags = diary.allTags
+    ? diary.allTags
+        .split(",")
+        .filter(
+          (tag) =>
+            ![
+              "매너타임",
+              "친절함",
+              "청결함",
+              "수영장",
+              "놀이시설",
+              "개별 화장실",
+              "개별 샤워실",
+              "매점 운영",
+            ].includes(tag)
+        )
+    : [];
+
   return (
     // <div className="diary-bg">
     <div
@@ -13,100 +84,56 @@ function DiaryView() {
         backgroundColor: "#ffffff",
         margin: "0 auto",
         height: "auto",
-        padding: "20px 32px 32px 32px",
+        padding: "90px 32px 32px 32px",
       }}
     >
       <div className="diary-Wrap">
         {/* 유저 정보 */}
-        <div className="diary-mb" style={{ marginBottom: "10px" }}>
-          <UserCard userMyCard={false} />
+        <div className="diary-mb">
+          <UserCard userMyCard={false} userEmail={diary.userEmail} />
         </div>
+
         {/* 캠핑장 간략정보 */}
         <div className="diary-mb">
           {/* <CampSelectCard isLink={"/camp/view/1"} /> */}
         </div>
 
         {/* 태그 1 */}
-        {/* <UserTags /> */}
-        <div
-          style={{
-            display: "flex",
-            gap: "10px",
-            marginBottom: "10px",
-            flexWrap: "wrap",
-          }}
-        >
-          <span className="userTag">
-            <Icon iconName="iconShowers" />
-            매너타임
-          </span>
-          <span className="userTag">
-            <Icon iconName="iconShowers" />
-            친절함
-          </span>
-          <span className="userTag">
-            <Icon iconName="iconShowers" />
-            청결함
-          </span>
-          <span className="userTag">
-            <Icon iconName="iconShowers" />
-            수영장
-          </span>
-          {/* <span className="userTag">
-            <Icon iconName="iconShowers" />
-            놀이시설
-          </span>
-          <span className="userTag">
-            <Icon iconName="iconShowers" />
-            개별 화장실
-          </span>
-          <span className="userTag">
-            <Icon iconName="iconShowers" />
-            개별 샤워실
-          </span>
-          <span className="userTag">
-            <Icon iconName="iconShowers" />
-            매점 운영
-          </span> */}
-        </div>
+        {userTags.length > 0 && (
+          <div className="diary-mb">
+            <UserTags selectedUserTags={userTags} onUserTagToggle={() => {}} />
+          </div>
+        )}
 
         {/* 컨텐츠 내용 */}
-        <div className="diary-mb">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-          nesciunt ab suscipit modi rerum recusandae pariatur totam consectetur
-          enim labore?
-        </div>
+        {diary.content && <div className="diary-mb">{diary.content}</div>}
 
         {/* 태그 2 S */}
-        <div
-          style={{
-            display: "flex",
-            gap: "10px",
-            marginBottom: "10px",
-          }}
-        >
-          <span className="hashTag">#오토캠핑</span>
-          <span className="hashTag">#오토캠핑</span>
-        </div>
+        {hashTags.length > 0 && (
+          <div className="diary-mb">
+            <HashTags selectedHashTags={hashTags} onHashTagToggle={() => {}} />
+          </div>
+        )}
+
         {/* 태그 2 E */}
 
         {/* 캠핑 이미지  */}
-        <div
-          className="diaryView-imgWrap"
-          // style={{
-          //   display: "flex",
-          //   justifyContent: "center",
-          //   alignItems: "center",
-          //   flexDirection: "column",
-          //   gap: "20px",
-          // }}
-        >
-          <img src={diary_camp_img} alt="캠핑 이미지" />
-          <img src={diary_camp_img} alt="" />
-          <img src={diary_camp_img} alt="" />
-          <img src={diary_camp_img} alt="" />
-          <img src={diary_camp_img} alt="" />
-        </div>
+        {diary.image_url && diary.image_url.length > 0 && (
+          <div
+            className="diaryView-imgWrap"
+            // style={{
+            //   display: "flex",
+            //   justifyContent: "center",
+            //   alignItems: "center",
+            //   flexDirection: "column",
+            //   gap: "20px",
+            // }}
+          >
+            {diary.image_url.map((url, index) => (
+              <img key={index} src={url} alt={`diary image ${index + 1}`} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
